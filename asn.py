d6f49405654fc2184fbcb43b433293ec28aa9de3
Michael Bauer <mihi@lo-res.org>
2012-08-26 13:11:23 +0200

    fix asn - what if no asn returned?

--- asn.py
+++ asn.py
@@ -9,9 +9,10 @@ ips=[x[0].split("/")[0] for x in cur.fetchall()]
 cw=cymruwhois.Client(memcache_host=None)
 lu=cw.lookupmany(ips)
 for l in lu:
-    cur.execute("select asn from asn where asn=%s"%l.asn)
-    r=cur.fetchone()
-    if not r:
-        cur.execute("insert into asn (asn,owner) values(%s,'%s')"%(l.asn,l.owner))
-    cur.execute("update client set asn=%s where ip='%s' and asn is Null"%(l.asn,l.ip))
+    if l.asn:
+        cur.execute("select asn from asn where asn=%s"%l.asn)
+        r=cur.fetchone()
+        if not r:
+            cur.execute("insert into asn (asn,owner) values(%s,'%s')"%(l.asn,l.owner))
+        cur.execute("update client set asn=%s where ip='%s' and asn is Null"%(l.asn,l.ip))
 sql.commit()    
